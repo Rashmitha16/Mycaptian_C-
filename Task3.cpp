ae246d821ef6ae23fc651e951ecb77144a124458
Rashmitha16 <43911325+Rashmitha16@users.noreply.github.com>
2019-05-23 21:57:29 +0530

    Add files via upload

---
+++ Task3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n;
+	cout<<"Enter a year";
+	cin>>n;
+	if(n%4==0)
+	{
+		cout<<"It is a leap year";
+	}
+	else
+	{
+		cout<<"It is not a leap year";
+	}
+}
